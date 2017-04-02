45da76537c0f17d5cfddbb6899abc6d9938f08ce
DrZoid <drzoid@tppt.eu>
2017-04-02 18:07:16 +0200

    Copy .hex from arduino build dir to firmware/

--- compile.sh
+++ compile.sh
@@ -1,1 +1,17 @@
-/home/drzoid/app/arduino-1.6.12/arduino --verbose --preserve-temp-files --board arduino:avr:leonardo --verify ./gbz-keyb-battmon/gbz-keyb-battmon.ino
+/home/drzoid/app/arduino-1.6.12/arduino --verbose --preserve-temp-files --board arduino:avr:leonardo --verify ./gbz-keyb-battmon/gbz-keyb-battmon.ino > /tmp/arduino_build.log
+
+DIR=`cat /tmp/arduino_build.log | grep -oP '/tmp/arduino_build_(\d*)' | head -1`
+HEX=`cat /tmp/arduino_build.log | grep -oP '/tmp/arduino_build_(\d*)/([^\s]*).hex'`
+
+#TODO: test dir et clean
+
+if [ ! -f $HEX ]; then
+  echo "Compilation failed, check /tmp/arduino_build.log"
+  exit -1
+fi
+
+echo "Copying $HEX to firmware/"
+
+cp $HEX firmware/
+
+echo "Done."
